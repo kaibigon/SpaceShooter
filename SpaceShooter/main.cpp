5f653f498d0263026d8a8f592df6b06d871773c2
Togem0n <413139475@qq.com>
2022-11-26 13:59:36 +0100

    Initial Commit

---
+++ SpaceShooter/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SpaceShooter
+//
+//  Created by Kai Wang on 2022/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
